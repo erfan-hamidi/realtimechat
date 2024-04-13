9eb1364a40f81356ba4a814c61633c893d224c02
Erfan <erfan9757@gmail.com>
2024-04-13 15:51:53 +0330

    complete chat view

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect 
+from django.contrib.auth.decorators import login_required
+from .models import *
 # Create your views here.
 
+@login_required
 def chat_view(req):
-    return render(req, 'a_rtchat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name="public chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(req, 'a_rtchat/chat.html', {'chat_messages' : chat_messages})
