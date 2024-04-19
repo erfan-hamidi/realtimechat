0673bc36c811597249b482ce7634d7219eaa9f7b
Erfan <erfan9757@gmail.com>
2024-04-19 17:06:13 +0330

    add handler

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -12,7 +12,25 @@ class ChatroomConsumer(WebsocketConsumer):
         self.user = self.scope['user']
         self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name'] 
         self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name,
+            self.channel_name 
+        )
+
         self.accept()
+
+
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
+        # remove and update online users
+        # if self.user in self.chatroom.users_online.all():
+        #     self.chatroom.users_online.remove(self.user)
+        #     self.update_online_count() 
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -24,6 +42,17 @@ class ChatroomConsumer(WebsocketConsumer):
             group = self.chatroom 
         )
 
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def message_handler(self, event):
+        message_id = event['message_id']
+        message = GroupMessage.objects.get(id=message_id)
         context = {
             'message': message,
             'user': self.user,
